4ab603655f8143514d5fac1d31900feb902189c3
AI-Tech-junkie <essadeq.billouche.com>
2023-07-13 19:05:39 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
-int main(){
+
+/**
+ * main - Entry point
+ * 
+ * Description: my first program ever 
+ * 
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
 	puts (" \"Programming is like building a multilingual puzzle"); 
 	return (0);
 }
-
