46772cb4d651b8a18a34223b958fc80806b163ed
AI-Tech-junkie <essadeq.billouche.com>
2023-07-13 19:24:10 -0700

    i fix it

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,12 @@
 
 /**
  * main - Entry point
- * 
- * Description: my first program ever 
- * 
+ * Description:my first program ever
  * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	puts (" \"Programming is like building a multilingual puzzle"); 
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
