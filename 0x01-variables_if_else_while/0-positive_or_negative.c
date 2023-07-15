3389b7f960d9e0eb1dd90854aa6fd1b24c2805d4
AI-Tech-junkie <essadeq.billouche.com>
2023-07-14 17:32:06 -0700

    first program

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entery point
+ *
+ * Description:This program will assign a random number.
+ *
+ * Return: (0) (success)
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%i is positive\n", n);
+	} else if (n < 0)
+	{
+		printf("%i is negative\n", n);
+	} else
+	{
+		printf("%i 0 is zero\n", n);
+	}
+	return (0);
+}
