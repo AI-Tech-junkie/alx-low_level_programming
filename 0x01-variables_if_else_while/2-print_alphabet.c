17a85c2f1a23fe9d20fa17881f30a9a1427ee0d6
AI-Tech-junkie <essadeq.billouche.com>
2023-07-14 18:30:27 -0700

    easy

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entery point
+ *
+ * Description: second program
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return 0;
+}
