b9c1bcd6f5b8333eb132127ac2ee48f34587de85
AI-Tech-junkie <essadeq.billouche.com>
2023-07-18 16:36:09 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description:task0
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	char c;
+
+	c = '_';
+	_putchar(c);
+	c = 'p';
+	_putchar(c);
+	c = 'u';
+	_putchar(c);
+	c = 't';
+	_putchar(c);
+	c = 'c';
+	_putchar(c);
+	c = 'h';
+	_putchar(c);
+	c = 'a';
+	_putchar(c);
+	c = 'r';
+	_putchar(c);
+	c = '\n';
+	_putchar(c);
+	return (0);
+}
