cde4c3ef8b39c7e72e695bbc87af2390f238607f
AI-Tech-junkie <essadeq.billouche.com>
2023-07-18 18:39:37 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,21 @@
 #include "main.h"
+/**
+ * print_alphabet - Entry point
+ *
+ *Description:task1
+ *
+ *Return: 0 Always (success)
+*/
 void print_alphabet(void)
 {
-        int i;
-        char j = '\n';
+	int i;
+	int j = '\n';
 
-        for (i = 'a';i <= 'z';i++)
-        {
-                _putchar(i);
-        }
-        _putchar(j);
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar(j);
 
 }
