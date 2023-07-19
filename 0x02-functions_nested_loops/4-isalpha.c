75987f21337c91d3fd4b24c997c467a6bc5d600e
AI-Tech-junkie <essadeq.billouche.com>
2023-07-18 19:48:42 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - check if a character is lowercase
+ *
+ *@x:check input function
+ *
+ *Return: returns 1 if 'x' lowercase Otherwise Always 0 (success)
+ *
+*/
+int _islower(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
