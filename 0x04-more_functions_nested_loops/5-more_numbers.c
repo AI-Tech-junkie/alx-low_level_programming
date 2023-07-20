95ab8c3143d102a06baa9ec8607e6d12fea5d487
AI-Tech-junkie <essadeq.billouche.com>
2023-07-20 09:17:53 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - print number without 2-4
+ *
+ * Return: Always void
+*/
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j <= 9; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+			{
+				_putchar('0' + i / 10);
+				_putchar('0' + i % 10);
+			} else
+				_putchar('0' + i);
+		}
+		_putchar('\n');
+	}
+}
