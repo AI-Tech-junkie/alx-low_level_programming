64dbde3a1bae2aea1c3b7dc8ae17aef3f6bcb2ca
AI-Tech-junkie <essadeq.billouche.com>
2023-07-20 09:36:45 -0700

    error

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,9 +15,8 @@ void more_numbers(void)
 			if (i >= 10)
 			{
 				_putchar('1');
-				_putchar('0' + i % 10);
-			} else
-				_putchar('0' + i);
+			}
+			_putchar('0' + i % 10);
 		}
 		_putchar('\n');
 	}
