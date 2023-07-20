5b43ba95f24af518aeaa4a0855151ed4170c4705
AI-Tech-Junkie <essadeq.billouche>
2023-07-20 02:48:35 +0100

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_to_98 - print from input to 98
+ *@x: the input number
+ *return: void
+*/
+void print_to_98(int x)
+{
+	int i;
+
+	for (t = x; t <= 98; t++)
+	{
+		printf(" %i,", t);
+	}
+
+}
