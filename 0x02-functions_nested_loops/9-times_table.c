22af3ec1be4e158cd543ea74388e9bcbfd326729
AI-Tech-Junkie <essadeq.billouche>
2023-07-20 01:49:38 +0100

    space2

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,70 +1,36 @@
 #include "main.h"
 
 /**
-
  * times_table - print times_table
-
  *
-
  * Return: void
-
  *
-
 */
-
 void times_table(void)
-
 {
-
         int i, j, r;
 
-
-
         for (i = 0; i <= 9; i++)
-
         {
-
-                _putchar('\n');
-
-                for (j = 0; j <= 9; j++)
-
-                {
-
-                        r = i * j;
-
-                        
-
-                        if (r <= 9){
-
-                                _putchar(' ');
-
-                                _putchar('0' + r );
-
-                        }            
-
-                        if (r >= 10)
-
-                        {
-
-                                _putchar('0' + r / 10);
-
-                                _putchar('0' + r % 10);
-
-                        } 
-
-                        if (j != 9)
-
-                        {
-
-                                _putchar(',');
-
-                              
-
-                        }
-
-                }
-
-        }
-
+	       	_putchar('\n');
+	       	for (j = 0; j <= 9; j++)
+		{
+			r = i * j;
+			if (r <= 9)
+			{
+				_putchar(' ');
+				_putchar('0' + r );
+			}
+			if (r >= 10)
+			{
+				_putchar('0' + r / 10);
+				_putchar('0' + r % 10);
+			}
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
 }
-
