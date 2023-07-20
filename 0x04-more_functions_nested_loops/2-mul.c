6d5e54cdb905446e0dbc0d9deaf146abd91646e9
AI-Tech-junkie <essadeq.billouche.com>
2023-07-20 08:36:42 -0700

    task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiple of two number
+ *@c:first number
+ *@x:second number
+ * Return: Always r
+*/
+int mul(int c, int x)
+{
+	int r;
+
+	r = c * x;
+	return (r);
+}
