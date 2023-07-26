8efc3a786871a3585bec87985822fc276d179021
AI-Tech-junkie <essadeq.billouche.com>
2023-07-25 19:00:10 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rev_string - Reverse a string
+ * @s: Input string to be reversed
+ * Return: Always void
+*/
+void rev_string(char *s)
+{
+	int i = 0;
+	int j = 0;
+	char t;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j <= i / 2; j++)
+	{
+		t = *(s + j);
+		*(s + j) = *(s - j - 1 + i);
+		*(s - j - 1 + i) = t;
+	}
+	_putchar('\n');
+}
