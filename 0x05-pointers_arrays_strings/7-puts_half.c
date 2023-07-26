d80c3b282fb295de5ab375394ba6d19c9e397eb9
AI-Tech-junkie <essadeq.billouche.com>
2023-07-26 07:39:28 -0700

    task7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - Print second half of the string
+ * @s: Input string to be print
+ * Return: Always void
+*/
+
+void puts_half(char *s)
+{
+	int i = 0;
+	int j;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	if ((i % 2) == 0)
+	{
+		for (j = (i / 2); j <= i; j++)
+		{
+			_putchar(*(s + j));
+		}
+	}
+	if ((i % 2) != 0)
+	{
+		for (j = (i - 1) / 2; j <= i; j++)
+		{
+			_putchar(*(s + j));
+		}
+	}
+	_putchar('\n');
+}
