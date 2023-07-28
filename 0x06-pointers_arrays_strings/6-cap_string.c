46ee35a4375b4d59e6f47f68f03939eaa52e390c
AI-Tech-junkie <essadeq.billouche.com>
2023-07-28 14:15:36 -0700

    task6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * cap_string - convert lowercase characters to uppercase after separators
+ * @text: input string
+ * Return: Always text
+ */
+char *cap_string(char *text)
+{
+	int i = 0, j = 0;
+	int sep[] = {32, 10, 9, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	while (text[j] != '\0')
+	{
+		for (i = 0; i <= 12; i++)
+		{
+			if (text[j] == sep[i])
+			{
+				if (text[j + 1] >= 'a' && text[j + 1] <= 'z')
+				{
+					text[j + 1] = text[j + 1] - 97 + 65;
+				}
+			}
+		}
+		j++;
+	}
+	return (text);
+}
