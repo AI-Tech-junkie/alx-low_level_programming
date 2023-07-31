028eab2fdc6d73debbeaf83c5613d340050ebab0
AI-Tech-junkie <essadeq.billouche.com>
2023-07-31 08:56:21 -0700

    0-main.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,7 +8,7 @@
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
+	unsigned int i = 0;
 
 	while (i < n)
 	{
@@ -17,4 +17,3 @@ char *_memset(char *s, char b, unsigned int n)
 	}
 	return (s);
 }
-
