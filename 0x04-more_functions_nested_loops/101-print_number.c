bf61d8df79f29e54951118cc6a363c193cf0506a
AI-Tech-junkie <essadeq.billouche.com>
2023-07-31 08:46:43 -0700

    0x07-pointers_arrays_strings

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_number - print a number u take
+ * @n: the int the function
+ * return : Always void
+*/
+void print_number(int n)
+{
+	_putchar('0' + n);
+	_putchar('\n');
+}
