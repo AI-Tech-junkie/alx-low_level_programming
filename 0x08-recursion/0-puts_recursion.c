354afcb87faf006aba9493b52e6f8e9ed733afc5
AI-Tech-junkie <essadeq.billouche.com>
2023-08-02 10:44:10 -0700

    #include main.h

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Print _puts recursion
+ * @s: Input string to be print
+ * Return: Always void
+*/
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
