e9aec5214193b107941386e526f2171b71dc97cf
AI-Tech-junkie <essadeq.billouche.com>
2023-08-04 16:25:51 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *@argc: argement counter
+ *@argv: argment of value
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s ", argv[0]);
+	return (0);
+}
