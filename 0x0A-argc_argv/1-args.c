c65d16c2646c9ce7569aea427445dade28fc2b6c
AI-Tech-junkie <essadeq.billouche.com>
2023-08-04 16:37:45 -0700

    task 0

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *@argv: argment of value
+ *@argc: argment counter
+ * Return: Always 0
+*/
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
