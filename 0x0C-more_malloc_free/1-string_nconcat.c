017d4917bd76cc8c7d5b9f4a584772fef8509277
AI-Tech-junkie <essadeq.billouche.com>
2023-08-10 15:50:31 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,7 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		i++;
 	while (s2[j] != '\0')
 		j++;
-	s3 = malloc(i + j);
+	s3 = malloc(i + j + 1);
 	if (s3 == NULL)
 		return (NULL);
 	for (s = 0; s < i; s++)
