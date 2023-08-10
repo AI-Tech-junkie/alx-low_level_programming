7845016a2a3f232f2636fec04dbe81d5681d08cd
AI-Tech-junkie <essadeq.billouche.com>
2023-08-10 15:19:48 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,10 +12,10 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s3;
-	int i = 0;
-	int j = 0;
-	int a;
-	int s;
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int a;
+	unsigned int s;
 
 	if (s1 == NULL)
 		s1 = "";
