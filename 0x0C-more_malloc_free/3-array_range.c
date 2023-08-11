eea72a15f3adbe8a7316f770fb841da2603dfe82
AI-Tech-junkie <essadeq.billouche.com>
2023-08-11 14:12:58 -0700

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,24 +1,25 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * array_range - function to print range
- *@min: min nuber
- *@max: max number
- * Return: allways s3 or NULL
+ * array_range - function to create an array of integers within a range
+ * @min: min number
+ * @max: max number
+ * Return: pointer to the dynamically allocated array, or NULL if failed
 */
 int *array_range(int min, int max)
 {
 	int *s3;
-	int i, j;
+	int size, i;
 
 	if (min > max)
 		return (NULL);
-	j = max - min + 1;
-	s3 = malloc(sizeof(int) * j);
+	size = max - min + 1;
+	s3 = malloc(sizeof(int) * size);
 	if (s3 == NULL)
 		return (NULL);
-	for (i = 0; i <= j; i++)
+	for (i = 0; i < size; i++)
 	{
 		s3[i] = min + i;
 	}
