33859d5ccbe82f8c512048accd9755566ca572d9
AI-Tech-junkie <essadeq.billouche.com>
2023-08-11 11:23:17 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - function to allocate memory
+ * @size: size of memory
+ * @s1: array size
+ * Return: allways s3 or NULL
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int *s3;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	s3 = malloc(nmemb * size);
+	if (s3 == NULL)
+		return (NULL);
+	return (s3);
+}
