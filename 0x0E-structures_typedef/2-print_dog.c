c55fb1aa63e121cb2a189067c461e388adae6320
AI-Tech-junkie <essadeq.billouche.com>
2023-08-14 09:50:34 -0700

    fixed?

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,8 +10,7 @@ void print_dog(struct dog *d)
 	if (d->name == NULL)
 		d->name = "(nil)";
 	printf("Name: %s \n", d->name);
-	if (d->age != 0)
-		printf("Age: %f \n", d->age);
+	printf("Age: %f \n", d->age);
 	if (d->owner != NULL)
 		printf("Owner: %s \n", d->owner);
 }
