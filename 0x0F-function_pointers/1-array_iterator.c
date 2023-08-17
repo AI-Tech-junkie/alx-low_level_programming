64bf55d7e959d6f3cb8e7108034dd768b3bd7321
AI-Tech-junkie <essadeq.billouche.com>
2023-08-16 17:31:21 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,9 +10,8 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i, j;
+	size_t i;
 
-	j = size;
-	for (i = 0; i < j; i++)
+	for (i = 0; i < size; i++)
 		action(*(array + i));
 }
