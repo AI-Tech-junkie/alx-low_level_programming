a92c93631267507b205602e81a426944317786e2
AI-Tech-junkie <essadeq.billouche.com>
2023-08-17 18:36:23 -0700

    task1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -24,7 +24,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		for (i = 0; i < n; i++)
 		{
-			if (i > 0)
+			if (i > 0 && i <= n - 1)
 				printf(", ");
 			printf("%d", va_arg(list_of_numbers, int));
 		}
