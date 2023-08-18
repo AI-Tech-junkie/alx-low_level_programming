e784f3c93f9bcfa5e0f4ca8bfd3ae6444d324080
AI-Tech-junkie <essadeq.billouche.com>
2023-08-17 18:32:33 -0700

    task1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print of numbers
+ * @n: how many numbers
+ * @separator: separator symbol
+ * Return: allways void
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list list_of_numbers;
+
+	va_start(list_of_numbers, n);
+	if (separator == NULL)
+	{
+		for (i = 0; i < n; i++)
+		{
+			printf("%d", va_arg(list_of_numbers, int));
+		}
+	} else
+	{
+		for (i = 0; i < n; i++)
+		{
+			if (i > 0)
+				printf(", ");
+			printf("%d", va_arg(list_of_numbers, int));
+		}
+	}
+	printf("\n");
+	va_end(list_of_numbers);
+}
