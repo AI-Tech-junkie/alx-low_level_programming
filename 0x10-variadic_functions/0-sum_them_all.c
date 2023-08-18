3e05e65627dfcb8215f7d08541dc2535e654903f
AI-Tech-junkie <essadeq.billouche.com>
2023-08-17 17:49:39 -0700

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,13 +9,15 @@
 */
 int sum_them_all(const unsigned int n, ...)
 {
-	int i;
-	int sum;
-
-	if (n == 0)
-		return (0);
+	unsigned int i;
+	int sum = 0;
 	va_list list_of_numbers;
 
+	if (n == 0)
+	{
+		va_end(list_of_numbers);
+		return (0);
+	}
 	va_start(list_of_numbers, n);
 	for (i = 0; i < n; i++)
 	{
