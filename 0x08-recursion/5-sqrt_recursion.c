09f02d88db60b4d985211dbaff1af89b6481347f
essadeq <essadeq@billouche.com>
2023-08-31 13:09:48 -0700

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _sqrt - find _sqrt
+ * @n: number from frist function
+ * @s: allways 1
+ * Return: the len s or -1 if negative
+*/
+
+int _sqrt(int n, int s)
+{
+	if (s * s == n)
+		return (s);
+	if (s > n / 2)
+		return (-1);
+	_sqrt(n, s + 1);
+}
+/**
+ * _sqrt_recursion - find _sqrt
+ * @n: number from frist function
+ * Return: the i or -1 if negative
+*/
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+	if (n <= 0)
+		return (-1);
+	i = _sqrt(n, 1);
+	return (i);
+}
