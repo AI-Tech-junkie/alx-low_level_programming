cbe4309d7f1c33017b7c043ec745824f95a7a7d5
essadeq <essadeq@billouche.com>
2023-09-05 16:51:34 -0700

    task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * str_concat - creat an array
+ * @s1: size of array
+ * @s2: charcter user whant
+ * Return: 0 if faill array if sucsess
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0;
+	char *conact_string;
+	int len_of_s1 = strlen(s1);
+	int len_of_s2 = strlen(s2);
+
+	conact_string = (char *)malloc(len_of_s2 + len_of_s1);
+	if (conact_string == NULL)
+	{
+		free(conact_string);
+		return (NULL);
+	}
+	for (i = 0; i < len_of_s1; i++)
+	{
+		*(conact_string + i) = s1[i];
+	}
+	for (i = len_of_s1; i <= len_of_s1 + len_of_s2; i++)
+	{
+		*(conact_string + i) = *s2;
+		s2++;
+	}
+	return (conact_string);
+}
