53e4908dffbe338b8dab7539606d54b80594dce2
essadeq <essadeq@billouche.com>
2023-09-05 16:29:38 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strdup - function _strdup
+ * @str: tring u give
+ * Return: 0 if faill string if sucsess
+ */
+char *_strdup(char *str)
+{
+	char *new_string;
+
+	new_string = malloc(strlen(str) * sizeof(char));
+	if (new_string == NULL || str)
+	{
+		free(new_string);
+		return (NULL);
+	}
+	strcpy(new_string, str);
+	return (new_string);
+}
