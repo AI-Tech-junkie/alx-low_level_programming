d42e96792dd35c39951959875d2d19daf384f42a
essadeq <essadeq@billouche.com>
2023-09-07 12:14:18 -0700

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * malloc_checked - allocated memory with malloc
+ * @b: size of array
+ * Return: pointer if it sucessed if faill exit
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *ptr_b;
+
+	ptr_b = malloc(sizeof(int) * b);
+	if (ptr_b == NULL)
+	{
+		exit (98);
+	}
+	return (ptr_b);
+}
+
+
+
