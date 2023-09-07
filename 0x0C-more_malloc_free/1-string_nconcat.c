5e394341fd1090735345f40dbc54c1f0f31f9c51
essadeq <essadeq@billouche.com>
2023-09-07 13:46:12 -0700

    1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * string_nconcat - contact 2 string
+ * @s1: size of array
+ * @s2: charcter user whant
+ * @n:number of char
+ * Return: NULL if faill array if sucsess
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ptr_return;
+	int i = 0;
+	int j = 0;
+	int len_s2;
+	int len_s1;
+
+	len_s1 = strlen(s1);
+	if (s2 == NULL)
+		s2 = "";
+	len_s2 = strlen(s2);
+	if (n <= len_s2)
+	{
+		len_s2 = n;
+	}
+	ptr_return = malloc(sizeof(char) * (len_s2 + len_s1) + 1);
+	if (ptr_return == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(ptr_return, s1);
+	for (i = len_s1; i < (len_s2 + len_s1); i++)
+	{
+		*(ptr_return + i) = s2[j];
+		j++;
+	}
+	*(ptr_return + (len_s2 + len_s1)) = '\0';
+	return (ptr_return);
+}
